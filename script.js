b2f3358660185fafc097be246e67a40e9ace4cee
Samar <Samharjai17@gmail.com>
2024-08-14 17:11:54 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById('contact-btn').addEventListener('click', function() {
+    document.getElementById('contact-form').style.display = 'block';
+});
+
+document.getElementById('close-form').addEventListener('click', function() {
+    document.getElementById('contact-form').style.display = 'none';
+});
